34926a8b1b12a8997acaa8698bd39abde76dd8bb
aravinth5 <71620683+aravinth5@users.noreply.github.com>
2021-07-02 10:47:23 +0530

    Check full square number
    This code is for check the given number is full square  number or not. 
    THANKYOU.

---
+++ check_full_square_number.py
@@ -0,0 +1,15 @@
+import math
+#getting input
+inp = int(input('enter number'))
+
+#get root value 
+root = math.sqrt(inp)
+
+#change to int
+changed = int(root)
+
+if root == int(root):
+  print(root)
+  
+else:
+  print('no')
